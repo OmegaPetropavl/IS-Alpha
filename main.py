f8f38541a7ebc62e42de7a364cd082a9e4b4f1ab
danya <danya_valiyev@mail.ru>
2023-04-06 17:05:12 +0600

    primer1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):sad
+class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1366, 768)
