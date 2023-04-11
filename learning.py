99e7f6bd902568601e50c9869a2e324550fb4382
danya <danya_valiyev@mail.ru>
2023-04-11 20:07:36 +0600

    main

--- learning.py
+++ learning.py
@@ -1,42 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-
-class Window1(QWidget):
+class LoginWindow(QWidget):
     def __init__(self):
-        super(Window1, self).__init__()
-        self.setWindowTitle('Window1')
-        self.setMinimumWidth(200)
-        self.setMinimumHeight(50)
-        self.button = QPushButton(self)
-        self.button.setText('Ok')
-        self.button.show()
+        super().__init__()
+        self.setWindowTitle('Окно регистрации')
 
+        self.label_username = QLabel('Имя пользователя')
+        self.label_password = QLabel('Пароль')
 
-class Window2(QWidget):
+        self.textbox_username = QLineEdit()
+        self.textbox_password = QLineEdit()
+        self.textbox_password.setEchoMode(QLineEdit.Password)
+
+        self.button_login = QPushButton('Войти')
+        self.button_login.clicked.connect(self.check_login)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_username)
+        layout.addWidget(self.textbox_username)
+        layout.addWidget(self.label_password)
+        layout.addWidget(self.textbox_password)
+        layout.addWidget(self.button_login)
+
+        self.setLayout(layout)
+
+    def check_login(self):
+        # здесь можно проверить правильность введенного логина и пароля
+        # если логин и пароль правильные, можно создать главное меню и открыть его
+        self.main_menu = MainMenuWindow()
+        self.main_menu.show()
+        login_window.close()
+
+class MainMenuWindow(QWidget):
     def __init__(self):
-        super(Window2, self).__init__()
-        self.setWindowTitle('Window2')
+        super().__init__()
+        self.setWindowTitle('Главное меню')
 
+        self.label_main_menu = QLabel('Это главное меню')
+        self.button_exit = QPushButton('Выход')
+        self.button_exit.clicked.connect(self.close)
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setWindowTitle('MainWindow')
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_main_menu)
+        layout.addWidget(self.button_exit)
 
-    def show_window_1(self):
-        self.w1 = Window1()
-        self.w1.button.clicked.connect(self.show_window_2)
-        self.w1.button.clicked.connect(self.w1.close)
-        self.w1.show()
-
-    def show_window_2(self):
-        self.w2 = Window2()
-        self.w2.show()
+        self.setLayout(layout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    w.show_window_1()
+    login_window = LoginWindow()
+    login_window.show()
     sys.exit(app.exec_())
--- login667.py
+++ login667.py
@@ -1,9 +1,10 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 import MySQLdb as mdb
 import mysql.connector as mc
 
-class Ui_MainWindow(object):
+class LoginBeg(QWidget):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(359, 373)
@@ -82,16 +83,92 @@ class Ui_MainWindow(object):
 
             if result1 == None and result2 == None:
                 self.labelResult.setText("Неправильный логин или пароль!")
-                self.close()
+                
+
             else:
                 self.labelResult.setText("Вход проведен успешно!")
+                self.main_menu = Ui_MainWindow()
+                
         except mdb.Error as e:
             error = QMessageBox()
             error.setText('Подключение невозможно!')
             error.exec_()
             
 
+class Ui_MainWindow():
+    def __init__(self):
+        super().__init__()
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(700, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 700, 22))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menubar)
+        self.menu_2.setObjectName("menu_2")
+        self.menu_3 = QtWidgets.QMenu(self.menubar)
+        self.menu_3.setObjectName("menu_3")
+        self.menu_4 = QtWidgets.QMenu(self.menubar)
+        self.menu_4.setObjectName("menu_4")
+        self.menu_5 = QtWidgets.QMenu(self.menubar)
+        self.menu_5.setObjectName("menu_5")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.action = QtWidgets.QAction(MainWindow)
+        self.action.setObjectName("action")
+        self.action_2 = QtWidgets.QAction(MainWindow)
+        self.action_2.setObjectName("action_2")
+        self.action_3 = QtWidgets.QAction(MainWindow)
+        self.action_3.setObjectName("action_3")
+        self.action_4 = QtWidgets.QAction(MainWindow)
+        self.action_4.setObjectName("action_4")
+        self.action_5 = QtWidgets.QAction(MainWindow)
+        self.action_5.setObjectName("action_5")
+        self.action_6 = QtWidgets.QAction(MainWindow)
+        self.action_6.setObjectName("action_6")
+        self.action_7 = QtWidgets.QAction(MainWindow)
+        self.action_7.setObjectName("action_7")
+        self.action_8 = QtWidgets.QAction(MainWindow)
+        self.action_8.setObjectName("action_8")
+        self.menu_2.addAction(self.action)
+        self.menu_2.addSeparator()
+        self.menu_2.addAction(self.action_8)
+        self.menu_3.addAction(self.action_3)
+        self.menu_3.addAction(self.action_4)
+        self.menu_3.addAction(self.action_5)
+        self.menu_4.addAction(self.action_6)
+        self.menu_5.addAction(self.action_7)
+        self.menubar.addAction(self.menu_2.menuAction())
+        self.menubar.addAction(self.menu_3.menuAction())
+        self.menubar.addAction(self.menu_4.menuAction())
+        self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_5.menuAction())
 
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "АИС \"Аренда помещений\""))
+        self.menu.setTitle(_translate("MainWindow", "Статистика"))
+        self.menu_2.setTitle(_translate("MainWindow", "Файл"))
+        self.menu_3.setTitle(_translate("MainWindow", "Справочники"))
+        self.menu_4.setTitle(_translate("MainWindow", "Отчеты"))
+        self.menu_5.setTitle(_translate("MainWindow", "Параметры"))
+        self.action.setText(_translate("MainWindow", "Создать договор..."))
+        self.action_2.setText(_translate("MainWindow", "Просмотреть договор..."))
+        self.action_3.setText(_translate("MainWindow", "Арендаторы"))
+        self.action_4.setText(_translate("MainWindow", "Объекты"))
+        self.action_5.setText(_translate("MainWindow", "Договоры"))
+        self.action_6.setText(_translate("MainWindow", "Сформировать отчет..."))
+        self.action_7.setText(_translate("MainWindow", "Сведения о разработчиках..."))
+        self.action_8.setText(_translate("MainWindow", "Редактировать договор..."))
    
 
 
@@ -99,7 +176,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = LoginBeg()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
